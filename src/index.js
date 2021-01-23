16b7984e0e63275b731b8e2db5e4786ab934ffa9
vladislavho <introwlad@gmail.com>
2021-01-23 16:11:17 +0300

    file: finished brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  for (const iterator of bracketsConfig) {
+    arr.push(iterator.join(''))
+  }
+  for (let index = 0; index < arr.length; index++) {
+    if (str.includes(arr[index])) {
+      str = str.replace(arr[index], '');
+      index = -1
+    }
+  }
+  if (str.length === 0) {
+    return true
+  } else {
+    return false
+  }
+};
